b61da1dba1f1b1b79a4225e0b391483aaf869f96
Fabio Nascimento <fabionascimento@smartbr.com>
2023-09-01 19:28:13 -0300

    feat: add bootstrap-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Stack } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
 function App() {
   return (
     <>
-      <div>Starting</div>
+      <h1>ddsds</h1>
+      <Stack direction="horizontal" gap={2}>
+        <Button variant="primary">Button as link</Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
+      </Stack>
     </>
   );
 }
