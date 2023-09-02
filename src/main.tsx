eb21ed91d7e40c44f744aa3a0e6d3424befa691c
Fabio Nascimento <fabionascimento@smartbr.com>
2023-09-02 08:11:24 -0300

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.tsx";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import AppointmentScheduling from "./pages/AppointmentScheduling.tsx";
+import ScheduleConsultation from "./pages/ScheduleConsultation.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route
+            path="/appointment-scheduling"
+            element={<AppointmentScheduling />}
+          />
+          <Route
+            path="/schedule-consultation"
+            element={<ScheduleConsultation />}
+          />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
